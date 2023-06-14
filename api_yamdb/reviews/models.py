f1ace31a6de4846f7f14761be835ce5fb1bec656
Дмитрий Крупин <thediman23@gmail.com>
2023-06-14 16:41:39 +0300

    Созданы модели Category, Genre, Title и админка

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+from . validators import validate_year
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Категория",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Жанр",
+    )
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Произведение",
+        help_text="Проверьте название произведения",
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name="Год выпуска",
+        validators=[validate_year],
+    )
+    description = models.TextField(verbose_name="Описание")
+    genre = models.ManyToManyField(
+        Genre,
+        related_name="titles",
+        blank=True,
+        verbose_name="Жанр",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name="titles",
+        blank=True,
+        null=True,
+        verbose_name="Категория",
+    )
+
+    class Meta:
+        ordering = ["-year"]
+
+    def __str__(self):
+        return self.name
